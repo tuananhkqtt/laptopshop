60be0a28c53fdea1fe23e038a1a5f5385607ccba
tuananhkqtt <tuananhkqtt@gmail.com>
2024-05-10 01:33:06 +0700

    init project

---
+++ src/test/java/com/tuananh/laptopshop/LaptopshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuananh.laptopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
