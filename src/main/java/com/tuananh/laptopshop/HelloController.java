ba85cca1296483136332c9f76dcd788fb7c3b92b
tuananhkqtt <tuananhkqtt@gmail.com>
2024-05-10 02:20:55 +0700

    spring security

--- src/main/java/com/tuananh/laptopshop/HelloController.java
+++ src/main/java/com/tuananh/laptopshop/HelloController.java
@@ -5,8 +5,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/")
     public String index() {
         return "Hello World from Spring Boot!";
     }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page!";
+    }
+
 }
--- src/main/java/com/tuananh/laptopshop/LaptopshopApplication.java
+++ src/main/java/com/tuananh/laptopshop/LaptopshopApplication.java
@@ -2,12 +2,16 @@ package com.tuananh.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+		ApplicationContext abc = SpringApplication.run(LaptopshopApplication.class, args);
+		for (String s : abc.getBeanDefinitionNames()) {
+			System.out.println(s);
+		}
 	}
 
 }
