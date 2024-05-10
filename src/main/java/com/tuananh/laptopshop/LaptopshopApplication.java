db693ecad6f838bbef3639a5c77eadf48ab408f0
tuananhkqtt <tuananhkqtt@gmail.com>
2024-05-10 16:23:15 +0700

    spring mvc

--- src/main/java/com/tuananh/laptopshop/LaptopshopApplication.java
+++ src/main/java/com/tuananh/laptopshop/LaptopshopApplication.java
@@ -2,16 +2,15 @@ package com.tuananh.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext abc = SpringApplication.run(LaptopshopApplication.class, args);
-		for (String s : abc.getBeanDefinitionNames()) {
-			System.out.println(s);
-		}
+
+		SpringApplication.run(LaptopshopApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/com/tuananh/laptopshop/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.tuananh.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.tuananh.laptopshop.domain.User;
+import com.tuananh.laptopshop.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = userService.handleHello();
+        model.addAttribute("title", test);
+        model.addAttribute("des", "This is a description.");
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User user) {
+        System.out.println(user);
+        return "hello";
+    }
+}
